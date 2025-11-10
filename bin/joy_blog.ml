c6fe13a687799cdfd624eb348e48d015a7a215ce
Joy A <joy.gold13@gmail.com>
2025-11-10 20:25:43 +0100

    yocaml path

--- bin/joy_blog.ml
+++ bin/joy_blog.ml
@@ -1,23 +1,13 @@
 open Yocaml
 
-let www = Path.rel [ "_www" ]
-let assets = Path.rel [ "assets" ]
-let content = Path.rel [ "content" ]
-let images = Path.(assets / "images")
-let css = Path.(assets / "css")
-let pages = Path.(content / "pages")
-let articles = Path.(content / "articles")
-let templates = Path.(assets / "templates")
-
+(* Helper functions *)
 let with_ext exts file =
   List.exists (fun ext -> Path.has_extension ext file) exts
 
 let is_markdown = with_ext [ "md"; "markdown"; "mdown" ]
 
-let track_binary = 
-  Sys.executable_name
-  |> Yocaml.Path.from_string
-  |> Pipeline.track_file
+let track_binary =
+  Sys.executable_name |> Yocaml.Path.from_string |> Pipeline.track_file
 
 (* DEBUG: Add debug logging *)
 let debug_log msg =
@@ -25,104 +15,100 @@ let debug_log msg =
   let+ () = track_binary in
   Printf.eprintf "DEBUG: %s\n%!" msg
 
-let copy_images =
-  let images_path = Path.(www / "images")
+let copy_images resolver =
+  let images_path = Resolver.Target.images resolver
   and where = with_ext [ "svg"; "png"; "jpg"; "gif" ] in
-  Batch.iter_files 
-    ~where images 
+  Batch.iter_files ~where
+    (Resolver.Source.images resolver)
     (Action.copy_file ~into:images_path)
 
-let create_css =
-  let css_path = Path.(www / "style.css") in
+let create_css resolver =
+  let css_path = Resolver.Target.style_css resolver in
+  let css = Resolver.Source.css resolver in
   Action.Static.write_file css_path
     Task.(
       track_binary
       >>> Pipeline.pipe_files ~separator:"\n"
-            Path.[ 
-              css / "reset.css"
-            ; css / "style.css" ])
+            Path.[ css / "reset.css"; css / "style.css" ])
 
-let create_page source =
-  let page_path =
-    source 
-    |> Path.move ~into:www 
-    |> Path.change_extension "html"
-  in
+let create_page resolver source =
+  let page_path = Resolver.Target.page resolver ~source in
+  let templates = Resolver.Source.templates resolver in
   let pipeline =
     let open Task in
     let+ () = track_binary
     and+ () = debug_log ("Creating page: " ^ Path.to_string source)
-    and+ apply_templates = 
-      Yocaml_jingoo.read_templates 
-        Path.[ templates / "page.html"
-             ; templates / "layout.html" ]
+    and+ apply_templates =
+      Yocaml_jingoo.read_templates
+        Path.[ templates / "page.html"; templates / "layout.html" ]
     and+ metadata, content =
       Yocaml_yaml.Pipeline.read_file_with_metadata
         (module Archetype.Page)
         source
     in
-    content 
+    content
     |> Yocaml_markdown.from_string_to_html
     |> apply_templates (module Archetype.Page) ~metadata
   in
   Action.Static.write_file page_path pipeline
 
-let create_pages =
+let create_pages resolver =
   let where = is_markdown in
-  Batch.iter_files ~where pages create_page
+  Batch.iter_files ~where
+    (Resolver.Source.pages resolver)
+    (create_page resolver)
 
-let create_article source =
-  let article_path =
-    source
-    |> Path.(move ~into:(www / "articles"))
-    |> Path.change_extension "html"
-  in
+let create_article resolver source =
+  let article_path = Resolver.Target.article resolver ~source in
+  let templates = Resolver.Source.templates resolver in
   let pipeline =
     let open Task in
     let+ () = track_binary
-    and+ () = debug_log ("Creating article: " ^ Path.to_string source ^ " -> " ^ Path.to_string article_path)
+    and+ () =
+      debug_log
+        ("Creating article: "
+        ^ Path.to_string source
+        ^ " -> "
+        ^ Path.to_string article_path)
     and+ templates =
       Yocaml_jingoo.read_templates
-        Path.[ templates / "article.html"
-             ; templates / "layout.html" ]
+        Path.[ templates / "article.html"; templates / "layout.html" ]
     and+ metadata, content =
       Yocaml_yaml.Pipeline.read_file_with_metadata
         (module Archetype.Article)
         source
     in
-    content 
+    content
     |> Yocaml_markdown.from_string_to_html
     |> templates (module Archetype.Article) ~metadata
   in
   Action.Static.write_file article_path pipeline
 
-let create_articles =
+let create_articles resolver =
   let where = is_markdown in
-  Batch.iter_files ~where articles create_article
+  Batch.iter_files ~where
+    (Resolver.Source.articles resolver)
+    (create_article resolver)
 
-let compute_link source =
-  let article_filename = 
-    source
-    |> Path.basename
-    |> Option.map (fun name -> Filename.chop_extension name ^ ".html")
-    |> Option.value ~default:"article.html"
-  in
-  Path.(rel ["articles"; article_filename])
+(* 
+   The key fix: compute_link now uses Path.move instead of basename
+   This follows the YOCaml documentation approach
+*)
+let compute_link resolver source =
+  source
+  |> Resolver.Target.article resolver ~source
+  |> Resolver.Server.from_target resolver
 
-let fetch_articles = 
-  Archetype.Articles.fetch 
-    ~where:is_markdown 
-    ~compute_link
+let fetch_articles resolver =
+  Archetype.Articles.fetch ~where:is_markdown
+    ~compute_link:(compute_link resolver)
     (module Yocaml_yaml)
-    articles
+    (Resolver.Source.articles resolver)
 
-let create_index = 
-  let source = Path.(content / "index.md") in
-  let index_path =
-    source 
-    |> Path.move ~into:www 
-    |> Path.change_extension "html"
-  in
+let create_index resolver =
+  let source = Resolver.Source.index resolver in
+  let index_path = Resolver.Target.index resolver in
+  let templates = Resolver.Source.templates resolver in
   let pipeline =
     let open Task in
     let+ () = track_binary
@@ -130,22 +116,19 @@ let create_index =
     and+ templates =
       Yocaml_jingoo.read_templates
         Path.
-          [ templates / "index.html"
+          [
+            templates / "index.html"
           ; templates / "page.html"
           ; templates / "layout.html"
           ]
-    and+ articles = fetch_articles
+    and+ articles = fetch_articles resolver
     and+ metadata, content =
       Yocaml_yaml.Pipeline.read_file_with_metadata
         (module Archetype.Page)
         source
     in
-    let metadata = 
-        Archetype.Articles.with_page
-           ~page:metadata 
-           ~articles
-    in
-    content 
+    let metadata = Archetype.Articles.with_page ~page:metadata ~articles in
+    content
     |> Yocaml_markdown.from_string_to_html
     |> templates (module Archetype.Articles) ~metadata
   in
@@ -156,13 +139,11 @@ module Feed = struct
   let title = "Joy's Beautiful Apple-themed Blog"
   let site_url = "https://Dev-JoyA.github.io/yocaml_blog"
   let feed_description = "My personal blog using YOCaml"
-  
-  let owner = 
-    Yocaml_syndication.Person.make 
-      ~uri:site_url 
-      ~email:"joy.gold13@gmail.com"
+
+  let owner =
+    Yocaml_syndication.Person.make ~uri:site_url ~email:"joy.gold13@gmail.com"
       "Joy Aruku"
-      
+
   let authors = Nel.singleton owner
 
   let article_to_entry (url, article) =
@@ -170,80 +151,65 @@ module Feed = struct
     let open Yocaml_syndication in
     let page = Article.page article in
     let title = Article.title article
-    and content_url = 
-      site_url ^ Path.to_string url
-    and updated = 
-      Datetime.make (Article.date article)
-    and categories = 
-      List.map Category.make (Page.tags page)
-    and summary = 
-      Option.map Atom.text (Page.description page) 
-    in
-    let links =
-      [ Atom.alternate content_url ~title ] 
-    in
-    Atom.entry 
-      ~links 
-      ~categories 
-      ?summary 
-      ~updated 
-      ~id:content_url 
+    and content_url = site_url ^ Path.to_string url
+    and updated = Datetime.make (Article.date article)
+    and categories = List.map Category.make (Page.tags page)
+    and summary = Option.map Atom.text (Page.description page) in
+    let links = [ Atom.alternate content_url ~title ] in
+    Atom.entry ~links ~categories ?summary ~updated ~id:content_url
       ~title:(Atom.text title) ()
 
   let make entries =
     let open Yocaml_syndication in
-    Atom.feed 
-      ~title:(Atom.text title)
+    Atom.feed ~title:(Atom.text title)
       ~subtitle:(Atom.text feed_description)
       ~updated:(Atom.updated_from_entries ())
-      ~authors 
-      ~id:site_url 
-      article_to_entry 
-      entries
+      ~authors ~id:site_url article_to_entry entries
 end
 
-let create_feed =
-  let feed_path = Path.(www / Feed.path)
+let create_feed resolver =
+  let feed_path = Resolver.Target.atom resolver
   and pipeline =
     let open Task in
     let+ () = track_binary
     and+ () = debug_log "Creating feed"
-    and+ articles = fetch_articles in
-    articles 
-    |> Feed.make 
-    |> Yocaml_syndication.Xml.to_string
+    and+ articles = fetch_articles resolver in
+    articles |> Feed.make |> Yocaml_syndication.Xml.to_string
   in
   Action.Static.write_file feed_path pipeline
 
-let program () =
+let program resolver () =
   let open Eff in
-  let cache = Path.(www / ".cache") in
-  Action.restore_cache cache 
-  >>= copy_images
-  >>= create_css
-  >>= create_pages
-  >>= create_articles
-  >>= create_index
-  >>= create_feed
+  let cache = Resolver.Target.cache resolver in
+  Action.restore_cache cache
+  >>= copy_images resolver
+  >>= create_css resolver
+  >>= create_pages resolver
+  >>= create_articles resolver
+  >>= create_index resolver
+  >>= create_feed resolver
   >>= Action.store_cache cache
 
 let () =
+  let resolver = Resolver.make () in
   Printf.eprintf "=== Yocaml Blog Build ===\n";
-  Printf.eprintf "WWW path: %s\n" (Path.to_string www);
-  Printf.eprintf "Content path: %s\n" (Path.to_string content);
-  Printf.eprintf "Articles path: %s\n" (Path.to_string articles);
-  Printf.eprintf "Templates path: %s\n" (Path.to_string templates);
-  Printf.eprintf "Expected article URLs: http://localhost:8000/articles/article-name.html\n\n";
-  
+  Printf.eprintf "WWW path: %s\n"
+    (Path.to_string (Resolver.Target.target resolver));
+  Printf.eprintf "Content path: %s\n"
+    (Path.to_string (Resolver.Source.content resolver));
+  Printf.eprintf "Articles path: %s\n"
+    (Path.to_string (Resolver.Source.articles resolver));
+  Printf.eprintf "Templates path: %s\n"
+    (Path.to_string (Resolver.Source.templates resolver));
+  Printf.eprintf
+    "Expected article URLs: http://localhost:8000/articles/article-name.html\n\n";
+
   match Sys.argv.(1) with
-  | "server" -> 
-    Printf.eprintf "Starting server at http://localhost:8000/\n";
-    Yocaml_unix.serve 
-       ~level:`Info 
-       ~target:www 
-       ~port:8000 
-       program
-  | _ | (exception _) -> 
-     Yocaml_unix.run 
-       ~level:`Debug 
-       program
+  | "server" ->
+      Printf.eprintf "Starting server at http://localhost:8000/\n";
+      Yocaml_unix.serve ~level:`Info
+        ~target:(Resolver.Target.target resolver)
+        ~port:8000 (program resolver)
+  | _
+  | (exception _) ->
+      Yocaml_unix.run ~level:`Debug (program resolver)
---
+++ bin/resolver.ml
@@ -0,0 +1,50 @@
+open Yocaml
+
+type t = { source : Path.t; target : Path.t; server_root : Path.t }
+
+let make ?(source_folder = Path.rel []) ?(target_folder = Path.rel [ "_www" ])
+    ?(server_root = Path.abs []) () =
+  { source = source_folder; target = target_folder; server_root }
+
+module Source = struct
+  let source { source; _ } = source
+
+  (* Assets *)
+  let assets r = Path.(source r / "assets")
+  let images r = Path.(assets r / "images")
+  let css r = Path.(assets r / "css")
+  let templates r = Path.(assets r / "templates")
+
+  (* Content *)
+  let content r = Path.(source r / "content")
+  let pages r = Path.(content r / "pages")
+  let articles r = Path.(content r / "articles")
+  let index r = Path.(content r / "index.md")
+end
+
+module Target = struct
+  let target { target; server_root; _ } = Path.relocate ~into:target server_root
+  let cache r = Path.(target r / ".cache")
+  let images r = Path.(target r / "images")
+  let style_css r = Path.(target r / "style.css")
+
+  let page r ~source =
+    let into = target r in
+    source |> Path.move ~into |> Path.change_extension "html"
+
+  let article r ~source =
+    let into = Path.(target r / "articles") in
+    source |> Path.move ~into |> Path.change_extension "html"
+
+  let index r = Path.(target r / "index.html")
+  let atom r = Path.(target r / "atom.xml")
+end
+
+module Server = struct
+  let server { server_root; _ } = server_root
+
+  let from_target r path =
+    let prefix = Target.target r in
+    let into = server r in
+    path |> Path.trim ~prefix |> Path.relocate ~into
+end
